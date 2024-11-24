d0c4c72ec07ba9f3703fd6c6f6e10f97bc956cba
Lukasz Byczynski <mail@lukaszbyczynski.com>
2024-11-24 19:08:33 +0100

    Add NIX scripts to setup a dev enviroment

---
+++ default.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> {}, pkgs_stable ? import <nixpkgs-stable> {} }:
+
+let
+in pkgs.mkShell rec {
+  name = "fourthwall";
+  buildInputs = [
+    pkgs.zulu17
+    pkgs.gradle
+	];
+}
