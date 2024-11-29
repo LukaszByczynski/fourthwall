7a01c3886e3594b3911ed8e1f4eab79b8c8c2d62
Lukasz Byczynski <mail@lukaszbyczynski.com>
2024-11-29 20:55:20 +0100

    Run gitea agent

--- shared/domain/build.gradle.kts
+++ shared/domain/build.gradle.kts
@@ -9,4 +9,5 @@ dependencies {
 
     implementation(kotlin("stdlib"))
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
 }
--- shared/domain/src/main/kotlin/com/fourthwall/shared/domain/events/events.kt
+++ shared/domain/src/main/kotlin/com/fourthwall/shared/domain/events/events.kt
@@ -1,6 +1,15 @@
 package com.fourthwall.shared.domain.events
 
+import kotlinx.datetime.LocalDate
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class MovieAddedCmd(val id: Int, val title: String, val imdbId: String)
+
+@Serializable
+data class ShowTimeAdded(
+    val movie_id: Int,
+    val date: LocalDate,
+    val hour: Int,
+    val minute: Int
+)
